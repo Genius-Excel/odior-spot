a470c910c68c0cf0ebff3b6ba82d752084355059
Genius-Excel <geniusexcel9@gmail.com>
2024-08-24 17:29:53 +0100

    Url path name for 'order_success' function added to urls module.

--- restaurant/odiorapp/urls.py
+++ restaurant/odiorapp/urls.py
@@ -3,7 +3,12 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.sample, name='home'),
-    path('book-test/', views.book_test, name='book-test'),
+    path('', views.home, name='home'),
+    path('order-food/', views.order_placing, name='order'),
+    path('contact-us/', views.contact_us, name='contact-us'),
+    path('about-us/', views.about_us, name='about-us'),
+    path('order-success/', views.order_success, name='order-success'),
+
+
     
 ]
