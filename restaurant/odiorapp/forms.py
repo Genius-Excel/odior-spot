6ab6b2368a0e21bf2d79be6738f79aa7a0ec0623
Genius-Excel <geniusexcel9@gmail.com>
2024-08-24 17:37:41 +0100

    'OrderRequestForm' class modified to use Bootstrap class for the form fields by default.

--- restaurant/odiorapp/forms.py
+++ restaurant/odiorapp/forms.py
@@ -6,3 +6,10 @@ class OrderRequestForm(forms.ModelForm):
     class Meta:
         model = OrderRequest
         fields = "__all__"
+
+
+    def __init__(self, *args, **kwargs):
+        super(OrderRequestForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'form-control'})
