6f222ef435e9b036f00f494b6a8109ba93612bd8
Genius-Excel <geniusexcel9@gmail.com>
2024-08-24 14:55:35 +0100

    Urls module modified to remove dummy url paths for testing stripe payment integrations.

--- restaurant/odiorapp/urls.py
+++ restaurant/odiorapp/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.sample, name='sample'),
+    path('', views.sample, name='home'),
     path('book-test/', views.book_test, name='book-test'),
     
 ]
